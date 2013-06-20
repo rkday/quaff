d76fea795eaf550e1093bda5957811786d7e7d5b
Rob Day <rkd@rkd.me.uk>
2013-06-20 19:05:28 +0100

    Example of threaded UAS for handling multiple calls

---
+++ thread-uas.rb
@@ -0,0 +1,18 @@
+require './quaff'
+
+c = Connection.new(5060)
+loop do
+cid = c.get_new_call_id
+Thread.new do
+call = Call.new(c, cid)
+
+call.recv_request("INVITE")
+call.send("100 Trying")
+call.send("180 Ringing")
+sleep 15
+call.send("200 OK", true)
+call.recv_request("ACK")
+call.recv_request("BYE")
+call.send("200 OK")
+end
+end
