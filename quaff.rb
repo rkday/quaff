7f7f8304d2566f0405b906496d415ed55e32d171
Robert Day <rkd@rkd.me.uk>
2013-07-03 18:57:51 +0100

    Adding initial code for UAC function

--- quaff.rb
+++ quaff.rb
@@ -193,14 +193,10 @@ class Call
         @t1, @t2 = 0.5, 32
     end
 
-    def recv_request(method)
+    def recv_something
         data = @cxn.get_new_message @cid
         @retrans = nil
-        unless data["message"].request? and data["message"].sip_method.to_s == method
-            raise
-        end
         @src = data['source']
-        #@port, @ip = data["source"][1], data["source"][3]
         @last_To = data["message"].header("To")
         @last_From = data["message"].header("From")
         @last_Via = data["message"].header("Via")
@@ -208,7 +204,23 @@ class Call
         data
     end
 
-    def send(code, retrans=nil)
+    def recv_request(method)
+        data = recv_something
+        unless data["message"].request? and data["message"].sip_method.to_s == method
+            raise
+        end
+        data
+    end
+
+    def recv_response(code)
+        data = recv_something
+        unless data["message"].response? and data["message"].status_code == code
+            raise
+        end
+        data
+    end
+
+    def send_response(code, retrans=nil)
         msg = "SIP/2.0 #{ code }\r
 Via: #{ @last_Via }\r
 From: #{ @last_From }\r
@@ -219,9 +231,51 @@ Contact: <sip:127.0.1.1:5060;transport=UDP>\r
 Content-Length: 0\r
 \r
 "
-    @cxn.send(msg, @src)
-    if retrans then
-        @retrans = true
+        send_something(msg, retrans)
+    end
+
+    def send_request(method, sdp=True, retrans=nil)
+
+# local IP
+# local port
+# local media IP
+# local media port
+# IPv4 vs. IPv6
+# UDP vs. TCP
+# remote IP
+# remote port
+# process ID
+# tag
+# SDP length
+# CSeq
+# Call-ID
+
+        sdp="v=0
+      o=user1 53655765 2353687637 IN IP[local_ip_type] [local_ip]
+      s=-
+      c=IN IP[media_ip_type] [media_ip]
+      t=0 0
+      m=audio [media_port] RTP/AVP 0
+      a=rtpmap:0 PCMU/8000"
+
+        msg = "INVITE sip:[service]@[remote_ip]:[remote_port] SIP/2.0
+      Via: SIP/2.0/[transport] [local_ip]:[local_port];branch=[branch]
+      From: sipp <sip:sipp@[local_ip]:[local_port]>;tag=[pid]Quaff[call_number]
+      To: sut <sip:[service]@[remote_ip]:[remote_port]>
+      Call-ID: [call_id]
+      CSeq: 1 INVITE
+      Contact: sip:sipp@[local_ip]:[local_port]
+      Max-Forwards: 70
+      Subject: Quaff SIP Test
+      Content-Type: application/sdp
+      Content-Length: [len]
+
+"
+
+    def send_something(msg, retrans)
+        @cxn.send(msg, @src)
+        if retrans then
+            @retrans = true
             Thread.new do
                 timer = @t1
                 sleep timer
