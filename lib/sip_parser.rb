c947c7908a199c1b37ef197001e836e47e90ee0c
Rob Day <rkd@rkd.me.uk>
2013-09-07 23:34:54 +0100

    Reduce use of regexps to improve performance

--- lib/sip_parser.rb
+++ lib/sip_parser.rb
@@ -32,82 +32,91 @@ end
         @buf = ""
         @msg = SipMessage.new
         @state = :blank
+  def parse_partial(data)
+    return nil if data.nil?
+    data.lines.each do |line|
+      line.rstrip!
+      if @state == :blank
+        parse_line_blank line
+      elsif @state == :parsing_body
+        parse_line_body line
+      else
+        parse_line_first_line_parsed line
+      end
     end
+    if @state == :done
+      return @msg
+    else
+      return nil
+    end
+  end
 
-    def parse_partial(data)
-        return nil if data.nil?
-        data.lines.each do |line|
-            if @state == :blank
-                parse_line_blank line
-            elsif @state == :parsing_body
-                parse_line_body line
-            else
-                parse_line_first_line_parsed line
-            end
-        end
-            if @state == :done
-                return @msg
-            else
-                return nil
-            end
-    end
+  def message_identifier(msg)
+    msg.header("Call-ID")
+  end
 
-    def message_identifier(msg)
-        msg.header("Call-ID")
+  def parse_line_blank line
+    if line == ""
+      # skip empty lines
+      return
+    else
+      parts = line.split " ", 3
+      if parts[2] == "SIP/2.0"
+        # Looks like a SIP request
+        @msg.method = parts[1]
+        @msg.requri = parts[2]
+        @state = :first_line_parsed
+        return
+      elsif parts[0] == "SIP/2.0"
+        # Looks like a SIP response
+        @msg.status_code = parts[2]
+        @msg.reason = parts[3] || ""
+        @state = :first_line_parsed
+        return
+      else
+        raise parts.inspect
+      end
     end
+    # We haven't returned, so looks like a malformed line
+    raise line.inspect
+  end
 
-    def parse_line_blank line
-        if line =~ %r!^([A-Z]+) (.+) SIP/2.0\r$!
-            @msg.type = :request
-            @msg.method = $1
-            @msg.requri = $2
-            @state = :first_line_parsed
-        elsif line =~ %r!^SIP/2.0 (\d+) (.+)\r$!
-            @msg.type = :response
-            @msg.status_code = $1
-            @msg.reason = $3 || ""
-            @state = :first_line_parsed
-        elsif line == "\r" or line == "\r\n"
-            # skip empty lines
-        else
-            raise line.inspect
-        end
+  def parse_line_first_line_parsed line
+    if line.start_with? " "
+      @msg.headers[@cur_hdr][-1] += " "
+      @msg.headers[@cur_hdr][-1] += line.lstrip
+      if $1 == "Content-Length"
+        @state = :got_content_length
+      else
+        @state = :middle_of_headers
+      end
+    elsif line.include? ":"
+      parts = line.split ":", 2
+      header_name, header_value = parts[0].rstrip, parts[1].lstrip
+      @msg.headers[header_name] ||= []
+      @msg.headers[header_name].push header_value
+      @cur_hdr = header_name
+      if header_name == "Content-Length"
+        @state = :got_content_length
+      else
+        @state = :middle_of_headers
+      end
+    elsif line == ""
+      if @state == :got_content_length and @msg.header("Content-Length").to_i > 0
+        @state = :parsing_body
+      else
+        @state = :done
+      end
+    else raise line.inspect
     end
+  end
 
-    def parse_line_first_line_parsed line
-        if line =~ /^\s+(.+)\r/
-            @msg.headers[@cur_hdr][-1] += " "
-            @msg.headers[@cur_hdr][-1] += $1
-            if $1 == "Content-Length"
-                @state = :got_content_length
-            else
-                @state = :middle_of_headers
-            end
-        elsif line =~ /^([-\w]+)\s*:\s*(.+)\r/
-            @msg.headers[$1] ||= []
-            @msg.headers[$1].push $2
-            @cur_hdr = $1
-            if $1 == "Content-Length"
-                @state = :got_content_length
-            else
-                @state = :middle_of_headers
-            end
-        elsif line == "\r" or line == "\r\n"
-            if @state == :got_content_length and @msg.header("Content-Length").to_i > 0
-               @state = :parsing_body
-            else
-               @state = :done
-            end
-        else raise line.inspect
-        end
+  def parse_line_body line
+    @msg.body << line
+    if line == "" or @msg.body.length >= @msg.header("Content-Length").to_i
+      @state = :done
     end
-
-    def parse_line_body line
-       @msg.body << line
-       if line == "\r" or @msg.body.length >= @msg.header("Content-Length").to_i
-         @state = :done
-       end
-    end
+  end
 
 end
 
