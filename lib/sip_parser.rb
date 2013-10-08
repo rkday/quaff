bba493f0fc0fb2619b00bf040832eb549b94db54
Rob Day <rkd@rkd.me.uk>
2013-10-08 16:07:53 +0100

    Whitespace

--- lib/sip_parser.rb
+++ lib/sip_parser.rb
@@ -99,7 +99,7 @@ class SipParser
 
 end
 
-def gen_nonce auth_pairs, username, passwd, method, sip_uri         
+def gen_nonce auth_pairs, username, passwd, method, sip_uri
   a1 = username + ":" + auth_pairs["realm"] + ":" + passwd
   a2 = method + ":" + sip_uri
   ha1 = Digest::MD5::hexdigest(a1)
@@ -111,11 +111,11 @@ end
 def gen_auth_header auth_line, username, passwd, method, sip_uri
   # Split auth line on commas
   auth_pairs = {}
-  auth_line.sub("Digest ", "").split(",") .each do |pair| 
+  auth_line.sub("Digest ", "").split(",") .each do |pair|
     key, value = pair.split "="
     auth_pairs[key.gsub(" ", "")] = value.gsub("\"", "").gsub(" ", "")
   end
-  digest = gen_nonce auth_pairs, username, passwd, method, sip_uri         
+  digest = gen_nonce auth_pairs, username, passwd, method, sip_uri
   return %Q!Digest username="#{username}",realm="#{auth_pairs['realm']}",nonce="#{auth_pairs['nonce']}",uri="#{sip_uri}",response="#{digest}",algorithm="#{auth_pairs['algorithm']}",opaque="#{auth_pairs['opaque']}"!
   # Return Authorization header with fields username, realm, nonce, uri, nc, cnonce, response, opaque
 end
